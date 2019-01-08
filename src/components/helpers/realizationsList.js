e6149ce43700c1691189237b09afb72c1efb8b95
Kamil Korczyński <k.korczynski@selleo.com>
2019-01-08 21:23:15 +0100

    comment some files

--- src/components/helpers/realizationsList.js
+++ src/components/helpers/realizationsList.js
@@ -9,10 +9,10 @@ import realization15 from '../../images/realizations/realization-15.jpg'
 import realization16 from '../../images/realizations/realization-16.jpg'
 import realization17 from '../../images/realizations/realization-17.jpg'
 import realization18 from '../../images/realizations/realization-18.jpg'
-import realization19 from '../../images/realizations/realization-19.jpg'
-import realization2 from '../../images/realizations/realization-2.jpg'
+// import realization19 from '../../images/realizations/realization-19.jpg'
+// import realization2 from '../../images/realizations/realization-2.jpg'
 import realization20 from '../../images/realizations/realization-20.jpg'
-import realization21 from '../../images/realizations/realization-21.jpg'
+// import realization21 from '../../images/realizations/realization-21.jpg'
 import realization22 from '../../images/realizations/realization-22.jpg'
 import realization23 from '../../images/realizations/realization-23.jpg'
 import realization24 from '../../images/realizations/realization-24.jpg'
@@ -22,12 +22,12 @@ import realization27 from '../../images/realizations/realization-27.jpg'
 import realization28 from '../../images/realizations/realization-28.jpg'
 import realization29 from '../../images/realizations/realization-29.jpg'
 import realization3 from '../../images/realizations/realization-3.jpg'
-import realization4 from '../../images/realizations/realization-4.jpg'
+// import realization4 from '../../images/realizations/realization-4.jpg'
 import realization5 from '../../images/realizations/realization-5.jpg'
 import realization6 from '../../images/realizations/realization-6.jpg'
-import realization7 from '../../images/realizations/realization-7.jpg'
+// import realization7 from '../../images/realizations/realization-7.jpg'
 import realization8 from '../../images/realizations/realization-8.jpg'
-import realization9 from '../../images/realizations/realization-9.jpg'
+// import realization9 from '../../images/realizations/realization-9.jpg'
 
 const realization1Obj = {
   img: <img src={realization1} alt="" />,
@@ -59,18 +59,18 @@ const realization17Obj = {
 const realization18Obj = {
   img: <img src={realization18} alt="" />,
 }
-const realization19Obj = {
-  img: <img src={realization19} alt="" />,
-}
-const realization2Obj = {
-  img: <img src={realization2} alt="" />,
-}
+// const realization19Obj = {
+//   img: <img src={realization19} alt="" />,
+// }
+// const realization2Obj = {
+//   img: <img src={realization2} alt="" />,
+// }
 const realization20Obj = {
   img: <img src={realization20} alt="" />,
 }
-const realization21Obj = {
-  img: <img src={realization21} alt="" />,
-}
+// const realization21Obj = {
+//   img: <img src={realization21} alt="" />,
+// }
 const realization22Obj = {
   img: <img src={realization22} alt="" />,
 }
@@ -98,24 +98,24 @@ const realization29Obj = {
 const realization3Obj = {
   img: <img src={realization3} alt="" />,
 }
-const realization4Obj = {
-  img: <img src={realization4} alt="" />,
-}
+// const realization4Obj = {
+//   img: <img src={realization4} alt="" />,
+// }
 const realization5Obj = {
   img: <img src={realization5} alt="" />,
 }
 const realization6Obj = {
   img: <img src={realization6} alt="" />,
 }
-const realization7Obj = {
-  img: <img src={realization7} alt="" />,
-}
+// const realization7Obj = {
+//   img: <img src={realization7} alt="" />,
+// }
 const realization8Obj = {
   img: <img src={realization8} alt="" />,
 }
-const realization9Obj = {
-  img: <img src={realization9} alt="" />,
-}
+// const realization9Obj = {
+//   img: <img src={realization9} alt="" />,
+// }
 
 export const realizationsList = [
   realization1Obj,
@@ -128,10 +128,10 @@ export const realizationsList = [
   realization16Obj,
   realization17Obj,
   realization18Obj,
-  realization19Obj,
-  realization2Obj,
+  // realization19Obj,
+  // realization2Obj,
   realization20Obj,
-  realization21Obj,
+  // realization21Obj,
   realization22Obj,
   realization23Obj,
   realization24Obj,
@@ -141,10 +141,10 @@ export const realizationsList = [
   realization28Obj,
   realization29Obj,
   realization3Obj,
-  realization4Obj,
+  // realization4Obj,
   realization5Obj,
   realization6Obj,
-  realization7Obj,
+  // realization7Obj,
   realization8Obj,
-  realization9Obj,
+  // realization9Obj,
 ]
