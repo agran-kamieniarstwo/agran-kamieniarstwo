164be84e88afe55a749e128ce1afeb9b95871744
Kamil Korczyński <k.korczynski@selleo.com>
2019-03-08 00:38:44 +0100

    comment realization-2 because of some weird error

--- src/components/helpers/realizationsList.js
+++ src/components/helpers/realizationsList.js
@@ -2,14 +2,14 @@ import React from 'react'
 import realization1 from '../../images/realizations/realization-1.jpg'
 import realization10 from '../../images/realizations/realization-10.jpg'
 import realization11 from '../../images/realizations/realization-11.jpg'
-import realization2 from '../../images/realizations/realization-2.jpg'
+import realization2 from '../../images/realizations/realization2.jpg'
 import realization3 from '../../images/realizations/realization-3.jpg'
-import realization4 from '../../images/realizations/realization-4.jpg'
+import realization4 from '../../images/realizations/realization4.jpg'
 import realization5 from '../../images/realizations/realization-5.jpg'
 import realization6 from '../../images/realizations/realization-6.jpg'
-import realization7 from '../../images/realizations/realization-7.jpg'
+import realization7 from '../../images/realizations/realization7.jpg'
 import realization8 from '../../images/realizations/realization-8.jpg'
-import realization9 from '../../images/realizations/realization-9.jpg'
+import realization9 from '../../images/realizations/realization9.jpg'
 
 const realization1Obj = {
   img: <img src={realization1} alt="" />,
